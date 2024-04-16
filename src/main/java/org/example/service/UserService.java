b1365557010816c77d1b05d4ca8009149bb0d1b6
Sergio <sergio-guitar@yandex.ru>
2024-04-16 18:20:03 +0300

    2 sem

---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,30 @@
+package org.example.service;
+
+import org.example.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.example.model.User;
+import org.example.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id){
+        userRepository.deleteById(id);
+    }
+}
